dc4d2267ad46f76020196716c3ab257df134dccf
Adam Moody <moody20@llnl.gov>
2018-05-06 14:49:34 -0700

    sync: update semantics of test and wait

--- src/axl.h
+++ src/axl.h
@@ -28,7 +28,8 @@ int AXL_Finalize (void);
 /* Create a transfer handle (used for 0+ files)
  * Type specifies a particular method to use
  * Name is a user/application provided string
- * Returns an ID to the transfer handle */
+ * Returns an ID to the transfer handle,
+ * Returns -1 on error */
 int AXL_Create (const char* type, const char* name);
 
 /* Add a file to an existing transfer handle */
@@ -37,12 +38,15 @@ int AXL_Add (int id, const char* source, const char* destination);
 /* Initiate a transfer for all files in handle ID */
 int AXL_Dispatch (int id);
 
-/* Test if a transfer has completed
- * Returns 1 if the transfer has completed */
+/* Non-blocking call to test if a transfer has completed,
+ * returns 1 if the transfer has completed,
+ * does not indicate whether transfer was successful,
+ * only whether it's done */
 int AXL_Test(int id);
 
 /* BLOCKING
- * Wait for a transfer to complete */
+ * Wait for a transfer to complete,
+ * and finalize the transfer */
 int AXL_Wait (int id);
 
 /* Cancel an existing transfer */
