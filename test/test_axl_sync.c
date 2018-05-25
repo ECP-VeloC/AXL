99d9540a1ef322d0e8adb451aba7ed0178ea1854
Elsa Gonsiorowski <gonsiorowski1@llnl.gov>
2018-05-25 15:30:14 -0700

    no per-file headers needed for MIT

--- test/test_axl_sync.c
+++ test/test_axl_sync.c
@@ -1,14 +1,3 @@
-/*
- * Copyright (c) 2009, Lawrence Livermore National Security, LLC.
- * Produced at the Lawrence Livermore National Laboratory.
- * Written by Adam Moody <moody20@llnl.gov>.
- * LLNL-CODE-411039.
- * All rights reserved.
- * KVTree was originally part of The Scalable Checkpoint / Restart (SCR) library.
- * For details, see https://sourceforge.net/projects/scalablecr/
- * Please also read this file: LICENSE.TXT.
-*/
-
 #include "test_axl.h"
 #include "test_axl_sync.h"
 
