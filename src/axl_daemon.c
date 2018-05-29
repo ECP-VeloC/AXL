dd24c4d608f76754d57cd707cc4eb50561434fea
Adam Moody <moody20@llnl.gov>
2018-05-29 16:07:02 -0700

    drop old copyright info

--- src/axl_daemon.c
+++ src/axl_daemon.c
@@ -1,15 +1,4 @@
 /*
- * Copyright (c) 2009, Lawrence Livermore National Security, LLC.
- * Produced at the Lawrence Livermore National Laboratory.
- * Written by Adam Moody <moody20@llnl.gov>.
- * LLNL-CODE-411039.
- * All rights reserved.
- * This file is part of The Scalable Checkpoint / Restart (SCR) library.
- * For details, see https://sourceforge.net/projects/scalablecr/
- * Please also read this file: LICENSE.TXT.
-*/
-
-/*
  * The axl_transfer program is a deamon process that AXL launches as
  * one process per compute node.  It sleeps in the background, waking
  * periodically to read the transfer.scrinfo file from cache, which
