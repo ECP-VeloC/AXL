2875f3af6eb2b30310a705e5ca042fb48ee49496
Adam Moody <moody20@llnl.gov>
2018-06-20 11:55:21 -0700

    refer to global axl_file_lists instead of passing map to backend

--- src/axl_async_daemon.h
+++ src/axl_async_daemon.h
@@ -4,10 +4,10 @@
 int axl_flush_async_init_daemon(const char* axld_path, const char* transfer_file);
 int axl_flush_async_finalize_daemon(void);
 
-int axl_flush_async_start_daemon(kvtree* map, int id);
-int axl_flush_async_test_daemon(kvtree* map, int id, double* bytes_total, double* bytes_written);
-int axl_flush_async_wait_daemon(kvtree* map, int id);
-int axl_flush_async_cancel_daemon(kvtree* map, int id);
+int axl_flush_async_start_daemon(int id);
+int axl_flush_async_test_daemon(int id, double* bytes_total, double* bytes_written);
+int axl_flush_async_wait_daemon(int id);
+int axl_flush_async_cancel_daemon(int id);
 
 int axl_flush_async_stop_daemon();
 
