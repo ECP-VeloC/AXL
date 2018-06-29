13bd246c69bcf9cc02c3cf46502a79f96042fc55
Adam Moody <moody20@llnl.gov>
2018-06-29 13:18:54 -0700

    async_daemo: wrong functions names in header

--- src/axl_async_daemon.h
+++ src/axl_async_daemon.h
@@ -1,14 +1,14 @@
 #ifndef AXL_ASYNC_DAEMON_H
 #define AXL_ASYNC_DAEMON_H
 
-int axl_flush_async_init_daemon(const char* axld_path, const char* transfer_file);
-int axl_flush_async_finalize_daemon(void);
+int axl_async_init_daemon(const char* axld_path, const char* transfer_file);
+int axl_async_finalize_daemon(void);
 
-int axl_flush_async_start_daemon(int id);
-int axl_flush_async_test_daemon(int id, double* bytes_total, double* bytes_written);
-int axl_flush_async_wait_daemon(int id);
-int axl_flush_async_cancel_daemon(int id);
+int axl_async_start_daemon(int id);
+int axl_async_test_daemon(int id, double* bytes_total, double* bytes_written);
+int axl_async_wait_daemon(int id);
+int axl_async_cancel_daemon(int id);
 
-int axl_flush_async_stop_daemon();
+int axl_async_stop_daemon();
 
 #endif /* AXL_ASYNC_DAEMON_H */
