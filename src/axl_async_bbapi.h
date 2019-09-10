770fe9441d8e47afe87c6f5589f3fb3f3b1f08f3
Elsa Gonsiorowski (Uranus) <gonsiorowski1@llnl.gov>
2019-09-09 17:12:47 -0700

    axl module groups

--- src/axl_async_bbapi.h
+++ src/axl_async_bbapi.h
@@ -4,6 +4,12 @@
 #define AXL_BBAPI_KEY_TRANSFERHANDLE ("BB_TransferHandle")
 #define AXL_BBAPI_KEY_TRANSFERDEF ("BB_TransferDef")
 
+/** \file axl_async_bbapi.h
+ *  \ingroup axl
+ *  \brief implementation of axl for IBM bbapi */
+
+/** \name bbapi */
+///@{
 int axl_async_init_bbapi(void);
 int axl_async_finalize_bbapi(void);
 int axl_async_create_bbapi(int id);
@@ -12,5 +18,5 @@ int axl_async_start_bbapi(int id);
 int axl_async_test_bbapi(int id);
 int axl_async_wait_bbapi(int id);
 int axl_async_cancel_bbapi(int id);
-
+///@}
 #endif //AXL_ASYNC_BBAPI_H
