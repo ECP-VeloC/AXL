21410580f388156ee594e69242f0425e8e17739c
Elsa Gonsiorowski (Uranus) <gonsiorowski1@llnl.gov>
2019-09-09 17:12:47 -0700

    axl module groups

--- src/axl_sync.h
+++ src/axl_sync.h
@@ -1,8 +1,14 @@
 #ifndef AXL_SYNC_H
 #define AXL_SYNC_H
 
+/** \file axl_sync.h
+ *  \ingroup axl
+ *  \brief implementation of axl's synchronous mode */
+
+/** \name sync */
+///@{
 int axl_sync_start(int id);
 int axl_sync_test(int id);
 int axl_sync_wait(int id);
-
+///@}
 #endif //AXL_SYNC_H
