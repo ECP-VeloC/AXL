33bb2808160d9377607efa2cc9625ee6652a55b3
Marty McFadden <mcfadden8@llnl.gov>
2023-10-02 11:41:18 -0700

    Updates: run some mpiGraph tests on rabbits

--- src/axl_internal.h
+++ src/axl_internal.h
@@ -66,7 +66,7 @@ extern int axl_rank;
 /* attaches function name, file name, and line number to error messages
  * https://gcc.gnu.org/onlinedocs/cpp/Variadic-Macros.html */
 #define AXL_ERR(format, ...) axl_err(format " @ %s %s:%d", ##__VA_ARGS__, __func__, __FILE__, __LINE__)
-#define AXL_DBG(level, format, ...) axl_dbg(level, format " @ %s %s:%d", ##__VA_ARGS__, __func__, __FILE__, __LINE__)
+#define AXL_DBG(level, format, ...) axl_dbg(level, " @ %s %s:%d\n  " format, __func__, __FILE__, __LINE__, ##__VA_ARGS__)
 
 /*
 =========================================
